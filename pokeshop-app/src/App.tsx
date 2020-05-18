5abbfa2953e8227492e459c810869739c85d1f8e
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-17 23:50:38 -0300

    adicionando scss ao projeto

--- pokeshop-app/src/App.tsx
+++ pokeshop-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { PokemonPage } from './page/PokemonShopPage';
