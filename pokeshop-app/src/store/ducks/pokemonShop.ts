1d4a02e74d4115c0a5b18dcbb37c3f4326ead463
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-17 23:17:29 -0300

    Criação de action que busca informações dos pokemões
    Action Usada para pegar as informações específicas de cada pokemon

--- pokeshop-app/src/store/ducks/pokemonShop.ts
+++ pokeshop-app/src/store/ducks/pokemonShop.ts
@@ -1,22 +1,34 @@
 import { AnyAction } from 'redux';
 import { PokemonManager } from '../../model/PokemonManager';
+import Axios from 'axios';
+import { PokemonResponse } from '../../model/API/PokemonResponse';
 
 const POKEMON_SHOP_LIST_DATA = 'pokemon_shop_list_data';
 const POKEMON_SHOP_INFO_DATA = 'pokemon_shop_info_data';
+const POKEMON_SHOP_SHOW_DATA = 'pokemon_shop_show_data';
 
 const pokemonManager = new PokemonManager();
 
-const INITIAL_STATE = {
+interface PokemonShopState {
+  pokemonList: any[];
+  pokemonShow: any[];
+  pokemonInfo: any[];
+}
+
+const INITIAL_STATE: PokemonShopState = {
   pokemonList: [],
+  pokemonShow: [],
   pokemonInfo: [],
 };
 
-export default (state = INITIAL_STATE, action: AnyAction) => {
+export default (state = INITIAL_STATE, action: AnyAction): PokemonShopState => {
   switch (action.type) {
     case POKEMON_SHOP_LIST_DATA:
       return { ...state, pokemonList: action.payload };
     case POKEMON_SHOP_INFO_DATA:
       return { ...state, pokemonInfo: action.payload };
+    case POKEMON_SHOP_SHOW_DATA:
+      return { ...state, pokemonShow: action.payload };
     default:
       return state;
   }
@@ -26,5 +38,19 @@ export const loadPokemonList = () => {
   return async (dispatch: any) => {
     const pokemonList = await pokemonManager.getPokemons();
     dispatch({ type: POKEMON_SHOP_LIST_DATA, payload: pokemonList });
+  };
+};
+
+export const loadPokemonShow = (pokemons: string[]) => {
+  return async (dispatch: any) => {
+    const pokemonRequests = pokemons.map((pokemon) => {
+      const pokemonInfo = Axios.get<PokemonResponse>(pokemon);
+      return pokemonInfo;
+    });
+    const response = await Promise.all(pokemonRequests);
+
+    const payload = response.map((resp) => resp.data);
+
+    return dispatch({ type: POKEMON_SHOP_SHOW_DATA, payload });
   };
 };
