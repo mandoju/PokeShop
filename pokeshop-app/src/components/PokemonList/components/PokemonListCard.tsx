8bb066259375fabe2e3f4e8a1fd1da84cc14db69
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-18 00:52:09 -0300

    mostrando preço aleatório do pokemon

--- pokeshop-app/src/components/PokemonList/components/PokemonListCard.tsx
+++ pokeshop-app/src/components/PokemonList/components/PokemonListCard.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './PokemonListCard.module.scss';
 import { CARD_BUY_BUTTON_TITLE } from '../../../constants/dictionary';
 
-export const PokemonListCard = (pokemon: any) => {
-  console.log(pokemon.pokemon);
+export const PokemonListCard = ({pokemon}: {pokemon: any}) => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.pokemonInfo}>
-          <img src={pokemon.pokemon.sprites.front_default} />
-          <p>{pokemon.pokemon.name}</p>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+          <p>{pokemon.price}</p>
         </div>
 
         <div className={styles['button-container']}>
