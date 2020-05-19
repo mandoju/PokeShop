2e4a3ccb5685e315003d11e8db18093e6bc4bd6c
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-19 02:49:59 -0300

    Criação do tema do site dragão

--- pokeshop-app/src/components/Cart/CartList.tsx
+++ pokeshop-app/src/components/Cart/CartList.tsx
@@ -6,7 +6,11 @@ import { ModalMessage } from '../Modal/ModalMessage';
 import { CardListBuyModal } from './components/CartListBuyModal';
 import { getPriceString } from '../../utils/getPriceString';
 import { resetCart } from '../../store/ducks/cart';
-import { CART_HEADER_TITLE, CART_TOTAL_LABEL, CART_BUY_BUTTON_TITLE } from '../../constants/dictionary';
+import {
+  CART_HEADER_TITLE,
+  CART_TOTAL_LABEL,
+  CART_BUY_BUTTON_TITLE,
+} from '../../constants/dictionary';
 
 export const CartList = () => {
   const cart = useSelector((state: RootStateType) => state.cart.cart);
@@ -14,14 +18,15 @@ export const CartList = () => {
 
   const [modalState, setModalState] = useState(false);
 
-  const totalPrice = cart.reduce((a, b) => a + Math.floor(b.price * 100), 0) / 100
+  const totalPrice =
+    cart.reduce((a, b) => a + Math.floor(b.price * 100), 0) / 100;
 
   return (
     <>
       <div className="pokemon-cart-container">
         <div className="pokemon-cart-inner-container">
           <div className="pokemon-cart-title-container">
-  <p className="pokemon-cart-title">{CART_HEADER_TITLE}</p>
+            <p className="pokemon-cart-title">{CART_HEADER_TITLE}</p>
           </div>
           <div className="pokemon-cart-list-container">
             {cart.map((item, idx) => (
@@ -30,22 +35,30 @@ export const CartList = () => {
           </div>
           <div className="pokemon-cart-total-container">
             <div className="pokemon-cart-total-label-container ">
-              <p className="pokemon-cart-total-label-text">{CART_TOTAL_LABEL}</p>
+              <p className="pokemon-cart-total-label-text">
+                {CART_TOTAL_LABEL}
+              </p>
             </div>
             <div className="pokemon-cart-total-price-container">
-              <p className="pokemon-cart-total-price-text">{getPriceString(totalPrice)}</p>
+              <p className="pokemon-cart-total-price-text">
+                {getPriceString(totalPrice,true)}
+              </p>
             </div>
           </div>
-          <div className="pokemon-cart-button-container"  onClick={() => setModalState(true)}>
-            <p className="pokemon-cart-button-text">
-              {CART_BUY_BUTTON_TITLE}
-            </p>
+          <div
+            className="pokemon-cart-button-container"
+            onClick={() => setModalState(true)}
+          >
+            <p className="pokemon-cart-button-text">{CART_BUY_BUTTON_TITLE}</p>
           </div>
         </div>
       </div>
       <ModalMessage
         showModal={modalState}
-        closeFunction={() => {dispatch(resetCart());setModalState(false);}}
+        closeFunction={() => {
+          dispatch(resetCart());
+          setModalState(false);
+        }}
       >
         <CardListBuyModal total={cart.length} price={totalPrice} />
       </ModalMessage>
