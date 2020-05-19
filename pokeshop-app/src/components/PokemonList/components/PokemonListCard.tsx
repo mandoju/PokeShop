e4f8f609a8d8b143b080d98001eef96aeebdb832
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-19 01:25:49 -0300

    ajustes para os pokémon que não possui imagens

--- pokeshop-app/src/components/PokemonList/components/PokemonListCard.tsx
+++ pokeshop-app/src/components/PokemonList/components/PokemonListCard.tsx
@@ -3,6 +3,7 @@ import { CARD_BUY_BUTTON_TITLE } from '../../../constants/dictionary';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../../store/ducks/cart';
 import { getPriceString } from '../../../utils/getPriceString';
+import { NO_IMAGE_PATH } from '../../../constants/image';
 
 export const PokemonListCard = ({ pokemon }: { pokemon: any }) => {
   const dispatch = useDispatch();
@@ -10,7 +11,7 @@ export const PokemonListCard = ({ pokemon }: { pokemon: any }) => {
     <div className={'pokemon-shop-list-card-container'}>
       <div className={'pokemon-shop-list-card'}>
         <div className={'pokemon-shop-list-card-info'}>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} className={"pokemon-shop-list-card-image"} />
+          <img src={pokemon.sprites.front_default || NO_IMAGE_PATH } alt={pokemon.name} className={"pokemon-shop-list-card-image"} />
           <div style={{display: 'flexbox'}}>
             <p className="pokemon-shop-list-card-name">{pokemon.name}</p>
             <p className="pokemon-shop-list-card-price">{getPriceString(pokemon.price, true)}</p>
