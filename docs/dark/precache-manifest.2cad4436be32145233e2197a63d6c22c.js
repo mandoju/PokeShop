22ca50f9b2d12b543557c0d937caec88a14e71a2
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2020-05-19 09:48:18 -0300

    adição dos sites ao docs

---
+++ docs/dark/precache-manifest.2cad4436be32145233e2197a63d6c22c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd85b2ca3e5962e21715b83be3dbee6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac5904c89ff522c708f0",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "a52f5be2fcf6bbc91744",
+    "url": "/static/css/main.6da1b3a4.chunk.css"
+  },
+  {
+    "revision": "ac5904c89ff522c708f0",
+    "url": "/static/js/2.2c4776a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c4776a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52f5be2fcf6bbc91744",
+    "url": "/static/js/main.cd40c5e7.chunk.js"
+  },
+  {
+    "revision": "f65d296d070684ee6048",
+    "url": "/static/js/runtime-main.f9741ab2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
